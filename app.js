dd6541cc4dd29e6f61c866b05cc5a9229fc5d775
Nurul055 <ainurull273@gmail.com>
2026-02-08 08:18:12 +0700

    Refactor app.js for improved structure and clarity

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 
 // 2. Konfigurasi Express & View Engine
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'Frontend/views'));
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
@@ -116,3 +116,4 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 module.exports = app; 
+
